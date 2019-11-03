f869071a26d55dbd544bdf8bdbe61288ac6e4b33
Luis Emmanuel Maya Rocha <maya.rocha.emmanuel@gmail.com>
2019-11-03 00:42:26 -0600

    Plantillas listas, sin lógica

---
+++ app/src/main/java/imagen/emm/cibo/Almacen_Actual.java
@@ -0,0 +1,13 @@
+package imagen.emm.cibo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Almacen_Actual extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_almacen__actual);
+    }
+}
---
+++ app/src/main/java/imagen/emm/cibo/Filtro_Productos.java
@@ -0,0 +1,13 @@
+package imagen.emm.cibo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Filtro_Productos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filtro__productos);
+    }
+}
